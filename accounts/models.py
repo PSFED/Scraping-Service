4b2267aed46c55233cee2953107ff27850a28b16
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-05-09 19:43:40 +0300

    Finished authorization and started scraping

--- accounts/models.py
+++ accounts/models.py
@@ -1,23 +1,54 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import UserManager
 from django.db import models
 
 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class MyUser(AbstractBaseUser):
     email = models.EmailField(
-        verbose_name="email address",
+        verbose_name="Емейл",
         max_length=255,
         unique=True,
     )
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False, verbose_name='Админ')
     city = models.ForeignKey(
-        "City", on_delete=models.SET_NULL, blank=True, null=True)
+        "scraping.City", on_delete=models.SET_NULL, blank=True, null=True)
     language = models.ForeignKey(
-        "Language", on_delete=models.SET_NULL, blank=True, null=True)
+        "scraping.Language", on_delete=models.SET_NULL, blank=True, null=True)
     send_email = models.BooleanField(default=True)
 
-    objects = UserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -34,3 +65,7 @@ class MyUser(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
