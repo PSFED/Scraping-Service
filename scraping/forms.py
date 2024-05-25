1f340d4e813f131e664e5a4df6d8ec7d1495bee9
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-05-25 16:46:56 +0300

    worked with views

--- scraping/forms.py
+++ scraping/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import City, Language
+from .models import City, Language, Vacancy
 
 
 class FindForm(forms.Form):
@@ -9,3 +9,24 @@ class FindForm(forms.Form):
     language = forms.ModelChoiceField(
         queryset=Language.objects.all(), to_field_name='slug', required=False,
         widget=forms.Select(attrs={'class': 'form-control'}), label='Специальность')
+
+
+class VForm(forms.ModelForm):
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}), label='Город')
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}), label='Специальность')
+    url = forms.CharField(widget=forms.URLInput(
+        attrs={'class': 'form-control'}), label='URL')
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}), label='Заголовок вакансии')
+    company = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}), label='Компания')
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control'}), label='Описание вакансии')
+
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
--- scraping/views.py
+++ scraping/views.py
@@ -1,8 +1,12 @@
+from typing import Any
 from django.core.paginator import Paginator
-from django.views.generic import DetailView
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404
 from .models import Vacancy
-from .forms import FindForm
+from .forms import FindForm, VForm
+from django.contrib import messages
 
 
 def home_view(request):
@@ -54,3 +58,59 @@ class VDetail(DetailView):
     # model = VDetail
     template_name = 'scraping/detail.html'
     # context_object_name = ''
+
+
+class VList(ListView):
+    model = Vacancy
+    template_name = 'scraping/detail.html'
+    form = FindForm()
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        qs = []
+
+        if city or language:
+            _filter = {}
+            if city:
+                _filter['city__slug'] = city
+            if language:
+                _filter['language__slug'] = language
+
+            qs = Vacancy.objects.filter(**_filter)
+        return qs
+
+
+class VCreate(CreateView):
+    model = Vacancy
+    fields = '__all__'
+    form_class = VForm
+    template_name = "scraping/create.html"
+    success_url = reverse_lazy('home')
+
+
+class VUpdate(UpdateView):
+    model = Vacancy
+    fields = '__all__'
+    form_class = VForm
+    template_name = "scraping/create.html"
+    success_url = reverse_lazy('home')
+
+
+class VDelete(DeleteView):
+    model = Vacancy
+    # template_name = "scraping/delete.html"
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, "Запись успешно удалена.")
+
+        return self.post(request, *args, **kwargs)
